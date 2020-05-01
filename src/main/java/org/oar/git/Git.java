ed05951860dff0ab692aa5b6670fbed650822873
subhasmitamishra310 <63075907+subhasmitamishra310@users.noreply.github.com>
2020-05-01 09:45:32 -0700

    yes it's done

--- src/main/java/org/oar/git/Git.java
+++ src/main/java/org/oar/git/Git.java
@@ -1,10 +1,8 @@
 package org.oar.git;
 
-public class Git {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class git{
+	public void m1()
+	{
+		System.out.println("hii");
 	}
-
 }
